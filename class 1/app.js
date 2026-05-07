d26b951aa92708019a8fd0b7f9562cf5b347b3bb
rafiapanjwani7-afk <rafiapanjwani7@gmail.com>
2026-05-07 20:34:12 +0500

    var,let,const

---
+++ class 1/app.js
@@ -0,0 +1,12 @@
+// bar bar redeclear inilize bhe ho skta sb ho skta
+var a = 20 
+a = 30;
+
+// let ko hum bar bar declear nhe krte 
+let b;
+b = 20;
+// declear krte waqt hi inialize krna hy 
+const c = 60;
+console.log(a,b,c);
+
+//modules Rest & Spread Operators Closures ye mene parhane hain friday
